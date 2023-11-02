1654f5e3f907a2b9bec38257bf19bb6a6ef115e5
MARC DIONIS DUMANGAS <marcdionis.dumangas.20@usjr.edu.ph>
2023-11-02 22:32:12 +0800

    Fixed character movement. Added object fade out when character is behind object

--- Assets/Character Implementation/Scripts/PlayerMovement.cs
+++ Assets/Character Implementation/Scripts/PlayerMovement.cs
@@ -4,6 +4,65 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private Rigidbody _instance;
+    [SerializeField] private float _speed = 5f;
+    [SerializeField] private float _turnSpeed = 360f;
+
+    private Vector3 _input;
+
+    public static PlayerMovement instance;
+   
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    void Update(){
+
+        GatherInput();
+        Look();
+
+    }
+
+    void FixedUpdate() {
+        Move();
+    }
+
+
+    void GatherInput(){
+        _input = new Vector3(Input.GetAxisRaw("Horizontal"),0, Input.GetAxisRaw("Vertical"));
+
+    }
+
+    void Look(){
+
+        if(_input != Vector3.zero){
+
+            var matrix = Matrix4x4.Rotate(Quaternion.Euler(0,45,0));
+
+            var skewedInput = matrix.MultiplyPoint3x4(_input);
+
+
+            var relative = (transform.position + skewedInput) - transform.position;
+
+            var rot = Quaternion.LookRotation(relative, Vector3.up);
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, _turnSpeed * Time.deltaTime);
+
+        }
+
+       
+    }
+
+
+    void Move(){
+        _instance.MovePosition(transform.position + (transform.forward * _input.magnitude) * _speed * Time.deltaTime);
+
+        float speed = _input.magnitude; // Calculate the magnitude of the movement input
+        //anim.SetFloat("Speed", speed); // Update the "Speed" animation parameter
+    }
+
+    /*
     public float speed;
     public float rotationSpeed;
     public float reposition = 2f;
@@ -38,4 +97,6 @@ public class PlayerMovement : MonoBehaviour
         Vector3 newPosition = transform.position + (forwardDirection * reposition);
         transform.position = newPosition;
     }
+
+    */
 }
