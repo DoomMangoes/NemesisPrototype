5282bb70f05bebd48b01f761724d81d4e2868192
Pacs007 <cbpacs007@gmail.com>
2023-12-11 08:45:45 +0800

    Adding Additional Changes
    - Bat Mob
    - Room Mobs
    - Enemy AI movement

--- Assets/Scripts/EnemyScripts/EnemyAI.cs
+++ Assets/Scripts/EnemyScripts/EnemyAI.cs
@@ -1,45 +1,50 @@
 using UnityEngine;
-using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
-    public NavMeshAgent agent;
-
     public Transform player;
-
     public LayerMask whatIsGround, whatIsPlayer;
-
     public float health;
 
-    //Patroling
+    // Patroling
     public Vector3 walkPoint;
     bool walkPointSet;
     public float walkPointRange;
+    public float patrolSpeed = 3f;
 
-    //Attacking
+    // Attacking
     public float timeBetweenAttacks;
     bool alreadyAttacked;
     public GameObject projectile;
 
-    //States
+    // States
     public float sightRange, attackRange;
     public bool playerInSightRange, playerInAttackRange;
+
+    // Animator
+    public Animator animator;
 
     private void Awake()
     {
         player = GameObject.Find("Player").transform;
-        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        //Check for sight and attack range
         playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
         playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        UpdateAnimator();
 
         if (!playerInSightRange && !playerInAttackRange) Patroling();
         if (playerInSightRange && !playerInAttackRange) ChasePlayer();
         if (playerInAttackRange && playerInSightRange) AttackPlayer();
+    }
+
+    private void UpdateAnimator()
+    {
+        animator.SetBool("isMoving", (walkPoint - transform.position).magnitude > 0.1f);
     }
 
     private void Patroling()
@@ -47,50 +52,60 @@ public class EnemyAI : MonoBehaviour
         if (!walkPointSet) SearchWalkPoint();
 
         if (walkPointSet)
-            agent.SetDestination(walkPoint);
+        {
+            // Move towards the walk point
+            transform.position = Vector3.MoveTowards(transform.position, walkPoint, patrolSpeed * Time.deltaTime);
 
+            // Rotate towards the walk point
+            transform.LookAt(walkPoint);
+        }
+
+        // Check if close to walk point
         Vector3 distanceToWalkPoint = transform.position - walkPoint;
-
-        //Walkpoint reached
         if (distanceToWalkPoint.magnitude < 1f)
             walkPointSet = false;
     }
+
     private void SearchWalkPoint()
     {
-        //Calculate random point in range
         float randomZ = Random.Range(-walkPointRange, walkPointRange);
         float randomX = Random.Range(-walkPointRange, walkPointRange);
 
         walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
 
-        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
-            walkPointSet = true;
+        // Assuming there's ground at the walk point's position
+        walkPointSet = true;
     }
 
     private void ChasePlayer()
     {
-        agent.SetDestination(player.position);
+        // Move towards the player
+        transform.position = Vector3.MoveTowards(transform.position, player.position, patrolSpeed * Time.deltaTime);
+
+        // Rotate towards the player
+        transform.LookAt(player);
     }
 
     private void AttackPlayer()
     {
-        //Make sure enemy doesn't move
-        agent.SetDestination(transform.position);
-
+        // Rotate towards the player
         transform.LookAt(player);
 
         if (!alreadyAttacked)
         {
-            ///Attack code here
+            // Attack code here
             Rigidbody rb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
             rb.AddForce(transform.forward * 32f, ForceMode.Impulse);
             rb.AddForce(transform.up * 8f, ForceMode.Impulse);
-            ///End of attack code
 
             alreadyAttacked = true;
             Invoke(nameof(ResetAttack), timeBetweenAttacks);
+
+            // Trigger the Attack animation
+            animator.SetTrigger("Attack");
         }
     }
+
     private void ResetAttack()
     {
         alreadyAttacked = false;
@@ -102,6 +117,7 @@ public class EnemyAI : MonoBehaviour
 
         if (health <= 0) Invoke(nameof(DestroyEnemy), 0.5f);
     }
+
     private void DestroyEnemy()
     {
         Destroy(gameObject);
